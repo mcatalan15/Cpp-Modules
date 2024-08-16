bb750074a33fcfb83aa3778a1e9ddb91f3d5065c
Marc Catalán Sánchez <marc.prat.2001@gmail.com>
2024-08-16 08:07:10 +0200

    adding cpp folders + headers

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/16 07:50:12 by mcatalan@st       #+#    #+#             */
-/*   Updated: 2024/08/16 07:50:13 by mcatalan@st      ###   ########.fr       */
+/*   Created: 2024/08/16 07:49:46 by mcatalan@st       #+#    #+#             */
+/*   Updated: 2024/08/16 07:49:47 by mcatalan@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cstring>
-
-int	main(int argc, char **argv)
-{
-	if (argc > 1)
-	{
-		for (int i = 1; argv[i]; i++)
-		{
-			for (size_t j = 0; argv[i][j]; j++)
-			{
-				argv[i][j] = std::toupper(argv[i][j]);
-				std::cout << argv[i][j];
-			}
-		}
-		std::cout << std::endl;
-	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	return 0;
-}
