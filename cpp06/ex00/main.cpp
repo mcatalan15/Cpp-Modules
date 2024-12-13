2f5380a81f668856bd21de52230ef8832520f0c5
Marc Catalán Sánchez <marc.prat.2001@gmail.com>
2024-12-13 12:11:51 +0100

    Recovering Cpp + errors comented

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 11:16:20 by mcatalan@st       #+#    #+#             */
-/*   Updated: 2024/12/09 11:23:03 by mcatalan@st      ###   ########.fr       */
+/*   Updated: 2024/12/13 12:09:43 by mcatalan@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 	main function with argv argument and tests comented. If argv num not 2 print
 	error message. And in case argv[1] is wrong, print the exception message.
 */
-
+/*
+	-negativos
+	-gestionar detras del num
+	-numero de puntos(decimales)
+*/
 int main(int argc, char **argv) {
 	if (argc == 2) {
 		try {
---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 12:11:14 by mcatalan@st       #+#    #+#             */
+/*   Updated: 2024/12/13 12:11:15 by mcatalan@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include <cstdlib>
+#include <ctime>
+/*
+	This function generates a randum number between 1 and 3.
+	Then it creates a new instance of A, B or C depending on the random number.
+*/
+Base *generate()
+{
+
+	srand(time(NULL));
+	int randomNumber = rand() % 3 + 1;
+	Base *base = NULL;
+	if (randomNumber == 1)
+		base = new A;
+	if (randomNumber == 2)
+		base = new B;
+	if (randomNumber == 3)
+		base = new C;
+	return (base);
+}
+/*
+	This function identifies the class from a pointer.
+	It uses dynamic_cast to check if the pointer is an instance of A, B or C.
+*/
+void identify(Base *p)
+{
+	std::cout << "Identify class from pointer" << std::endl;
+	A *a = dynamic_cast<A *>(p);
+	B *b = dynamic_cast<B *>(p);
+	C *c = dynamic_cast<C *>(p);
+	if (a)
+		std::cout << "Class A" << std::endl;
+	else if (b)
+		std::cout << "Class B" << std::endl;
+	else if (c)
+		std::cout << "Class C" << std::endl;
+	// delete a;
+	// delete b;
+	// delete c;
+}
+/*
+	This function identifies the class from a reference.
+	It uses dynamic_cast to check if the reference is an instance of A, B or C.
+*/
+void identify(Base &p)
+{
+	std::cout << "Identify class from reference" << std::endl;
+	if (dynamic_cast<A *>(&p))
+		std::cout << "Class A" << std::endl;
+	else if (dynamic_cast<B *>(&p))
+		std::cout << "Class B" << std::endl;
+	if (dynamic_cast<C *>(&p))
+		std::cout << "Class C" << std::endl;
+}
+/*
+	Main function that generates a random class and identifies it.
+*/
+int main()
+{
+	Base *base = generate();
+	identify(base);
+	identify(*base);
+	delete base;
+	return (0);
+}
